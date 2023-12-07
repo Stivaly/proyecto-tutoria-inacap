db3b2a26c027a23c4f0fe0dea13c31e2067d49c2
Stivaly Gomez <133545882+Stivaly@users.noreply.github.com>
2023-12-07 16:56:53 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,15 @@
+"""
+Script de tutoría para el programa de tutorías de INACAP.
+
+Este script tiene la finalidad de solventar un inconveniente que está enfrentando
+el equipo de tutorías del Intituto INACAP. Se necesita recibir con mayor prioridad
+alertas de los alumnos que se encuentran en riesgo de reprobar el ramo.
+
+El objetivo es detectar que alumnos tienen 2 notas reprobadas en una asignatura especifica.
+
+"""
+
+
+# Iniciación del programa
+if __name__ == '__main__':
+    pass
